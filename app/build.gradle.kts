970d1f61a0d915b1ed03ee382bae27dd829dae46
clarkarch <kentclark321123@gmail.com>
2026-06-25 15:26:19 +0800

    Phase 2: Add kotlin-stdlib version constraint to fix 2.4.0 conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,4 +105,13 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    constraints {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib") {
+            version {
+                strictly("[2.2.0, 2.3.0[")
+            }
+            because("kotlin-stdlib 2.4.0 is incompatible with Kotlin compiler 2.2.0")
+        }
+    }
 }
